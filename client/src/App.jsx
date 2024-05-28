6c6b2875e3b57f48ab8a600664f2a3522d31892f
Av1 <avanish47m@gmail.com>
2024-05-28 14:16:59 +0530

    install react js and tailwind css and creat first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function App() {
+  return (
+    <h1 className = 'text-3xl text-green-500'> Hello world! I am coming to rule over all the world. I am here to change the mindset of the people that they could do anything 
+      if they have faith inside them otherwise they are nothing without the trust , our mind is programmed and if we are unable to take the risk 
+      then we are going to loss our time , do not become self centered because in this world ony you are not existing other people are also exist 
+      so we have freedom, we have no power to do what we are thinking and and other people get disturbed by you ,this is nothing but creating a problem
+      in our socity and in our world . Thank you for listening me.  </h1>
+  )
+}
+
